b5f0a5adec00dc201733396bee100d15ba2f058a
Ricardo Ribeiro <richy.jsr@hotmail.com>
2023-06-16 19:41:00 +0100

    Your In When Scroll Reaches The End

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,34 +1,56 @@
 "use client";
 import Link from "next/link";
 import { motion, useScroll } from "framer-motion";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 
 export default function Navbar() {
 const navigation = [
     {
     name: "Our Goal",
-    href: "#",
+    href: "#OurGoal",
     },
     {
     name: "Manifesto",
-    href: "#",
+    href: "#Manifesto",
     },
     {
     name: "Why You",
-    href: "#",
+    href: "#WhyYou",
     },
     {
     name: "Book Call",
-    href: "#",
+    href: "#BookCall",
     },
 ];
 
 const { scrollYProgress } = useScroll();
+const [scrollY, setScrollY] = useState(0);
+
+const [pageEnd, setPageEnd] = useState(false);
+
+useEffect(() => {
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const handleScroll = () => {
+        setScrollY(window.scrollY);
+    };
+
+    if(scrollY >= height){ // verifies if the page reches the end
+        setPageEnd(true)
+    }else{
+        setPageEnd(false)
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+        window.removeEventListener("scroll", handleScroll);
+    };
+}, [pageEnd, scrollY]);
 
 return (
     <div className="border-b-[1px] border-white absolute w-full left-0 lg:h-[120px] md:h-[80px] h-[40px] flex flex-col justify-center">
     <motion.div
-        className="progress-bar lg:h-[120px] md:h-[80px] h-[40px] lg:ml-[120px] md:ml-[80px] ml-[40px] md:mr-[200px] mr-[120px]"
+        className="progress-bar lg:h-[120px] md:h-[80px] h-[40px] lg:ml-[120px] md:ml-[80px] "
         style={{ scaleX: scrollYProgress }}
     />
     <nav>
@@ -40,7 +62,8 @@ return (
         ))}
         </div>
     </nav>
-    <div className="bg-[#8870BB] flex items-center justify-center absolute right-0 top-0 h-full md:w-[200px] w-[120px]">
+    <div className={`flex items-center justify-center absolute right-0 top-0 h-full md:w-[200px] w-[120px] rounded-tr-[8px] transition-all duration-250 
+    ${pageEnd == true ? "opacity-1 scale-100":"opacity-0 scale-50"}` }>
         <div className="primary_font md:text-[26px] text-[18px]">Your In</div>
     </div>
     </div>
